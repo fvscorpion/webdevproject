80405f95d81b7c5f69f81558675dd02649e55ec4
Denis Bondarenko <fvscorpion2@gmail.com>
2019-04-29 17:20:16 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,35 @@
+$(function() {
+	$(".toggle-mnu").click(function() {
+		if (($(window).width()) < 810) {
+			$(this).toggleClass("on");
+			$(".menu").slideToggle();
+		};
+		return false;
+	});
+	  
+	$('.menu a').on("click", function() {
+		if (($(window).width()) < 810) {
+			$('.toggle-mnu').toggleClass('on');
+			$(".menu").slideToggle();
+		};
+	});
+
+	$(".menu").on("click", "a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+	$('.toggles button').click(function() {
+		var get_id = this.id;
+		var get_current = $('.latest-works .' + get_id);
+
+		$('.work').not(get_current).hide(500);
+		get_current.show(500);
+	});
+
+	$('#all').click(function() {
+		$('.work').show(500);
+	});
+});
